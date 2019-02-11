93da950b5b35da2d45b33335b4ceaf296851291d
Maksym <drayv.mj12@gmail.com>
2019-02-11 18:42:31 +0200

    Init

---
+++ LikesApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LikesApp.Startup))]
+namespace LikesApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
